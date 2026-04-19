78592c62e4b41197456a9084bd0b106d31ece9b3
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-04-20 00:00:11 +0530

    small change in auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -3,4 +3,22 @@ import 'package:get_storage/get_storage.dart';
 
 class AuthController extends GetxController{
   final _storage = GetStorage();
+
+  final RxBool _isFirstTime = true.obs;
+  final RxBool _isLoggedIn = false.obs;
+
+  bool get isFirstTime => _isFirstTime.value;
+  bool get isLoggedIn => _isLoggedIn.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadInitialState();
+  }
+
+  void _loadInitialState() {
+    _isFirstTime.value = _storage.read('isFirstTime')?? true;
+    _isLoggedIn.value = _storage.read('isLoggedIn') ?? false;
+  }
+
 }
