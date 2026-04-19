e3c9a0e9107c2dc736c2c722bc5341c7be0950dd
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-04-19 23:46:49 +0530

    auth controller WIP

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -5,6 +5,7 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // navigation based on auth state after 2.5 seconds
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -96,6 +97,32 @@ class SplashScreen extends StatelessWidget {
                     )
                   ),
                 ],
+              ),
+            ),
+            // bottom tagline
+            Positioned(
+              bottom: 48,
+              left: 0,
+              right: 0,
+              child: TweenAnimationBuilder<double>(
+                tween: Tween(begin: 0.0, end: 1.0),
+                duration: const Duration(milliseconds: 1200),
+                builder: (context, value, child){
+                  return Opacity(
+                    opacity: value,
+                    child: child,
+                  );
+                },
+                child: Text(
+                  'Style Meets Simplicity',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.9),
+                      fontSize: 14,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.w300
+                  ),
+                ),
               ),
             ),
           ],
