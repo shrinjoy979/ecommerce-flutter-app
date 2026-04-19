5f3c10a23f50f378fc67cb3b1f5b49ed7f65e943
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-04-19 23:25:08 +0530

    add splash screen WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,28 @@
+import 'package:ecommerce/controllers/theme_controller.dart';
+import 'package:ecommerce/utils/app_themes.dart';
+import 'package:ecommerce/view/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  Get.put(ThemeController());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final themeController = Get.find<ThemeController>();
+    return GetMaterialApp(
+      title: 'Fashion Store',
+      theme: AppThemes.light,
+      themeMode: themeController.theme,
+      defaultTransition: Transition.fade,
+      home: const SplashScreen(),
     );
   }
 }
